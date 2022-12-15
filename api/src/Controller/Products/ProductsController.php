b38bad43b3c29cf5683eb8360f524695826338ce
Dragomir Marjanovic <marjanovicdragomir@gmail.com>
2022-12-15 01:14:41 +0100

    Adding a new service logic

---
+++ api/src/Controller/Products/ProductsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Products;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\Products\GetProductsService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ProductsController extends AbstractController
+{
+    private $products;
+
+    /**
+     * @param GetProductsService $products
+     */
+    public function __construct(GetProductsService $products){
+    
+        $this->products = $products;
+
+    }
+
+    /**
+     * @Route("/api/products/", methods={"GET"})
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+       $productsAll = $this->products->execute();
+    
+       return new JsonResponse (
+        [
+        'products' => $productsAll,
+        ]);
+    }
+}
