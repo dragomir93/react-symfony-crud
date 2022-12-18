5cdcf01f82280fd936a22073e9e62eac8de268e7
Dragomir Marjanovic <marjanovicdragomir@gmail.com>
2022-12-18 01:11:05 +0100

    Adding first React app parts (first routes,lazyloading)

--- client/src/views/public/Products/Products.jsx
+++ client/src/views/public/Products/Products.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import Button from '@mui/material/Button';
 const Products = () => {
     return (
-        <div>Product test component</div>
+        <div>
+          <p>Product test component</p>
+          <Button variant="contained">Test Button</Button>
+        </div>
     );
 }
 
