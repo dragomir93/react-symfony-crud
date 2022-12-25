b729c7a78fa41e2a84fd28512265a9362cd14da7
Dragomir Marjanovic <marjanovicdragomir@gmail.com>
2022-12-25 16:58:38 +0100

    Adding route for edit

--- client/src/routes/PublicRoutes.js
+++ client/src/routes/PublicRoutes.js
@@ -13,6 +13,7 @@ const PublicRoutes = () => (
         element={<Navigate to={publicRouteCodes.PRODUCTS} replace />} />
     <Route path={publicRouteCodes.PRODUCTS} element={<Products/>} />
     <Route path={publicRouteCodes.PRODUCTS_CREATE} element={<ProductsForm/>} />
+    <Route path={publicRouteCodes.PRODUCTS_EDIT} element={<ProductsForm/>} />
   </Routes>
   </PublicTemplates>
 );
